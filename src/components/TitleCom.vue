24f42d9ace4c14c16c4c3e87c6a02ea9903f34b3
ych <185103177@qq.com>
2022-03-10 23:08:28 +0800

    master

---
+++ src/components/TitleCom.vue
@@ -0,0 +1,49 @@
+<template>
+<div class="title-box">
+    <div class="left">
+<img src="../assets/logo.png" alt="">
+    <h4>招聘网站</h4>
+    </div>
+    <div class="right">
+        <slot></slot>
+</div>
+</div>
+
+
+
+</template>
+
+<script>
+import {defineComponent } from 'vue'
+
+export default defineComponent ({
+    setup(){
+        return{}
+    }
+
+
+})
+</script>
+
+<style lang="scss" scoped>
+.title-box{
+    height: 100px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    border-bottom: 2px solid #999;
+    .left{
+    display: flex;
+    align-items: center;
+    img{
+        height: 70px;
+    }
+        
+    }
+    
+}
+
+
+
+</style>
---
+++ src/views/Index.vue
@@ -0,0 +1,42 @@
+<template>
+<div class="box">
+    <TitleCom>
+        <div class="btn">招聘广场</div>
+    </TitleCom>
+    <div class="select-box">
+        <div class="search-box">
+            <input type="text" placeholder="招聘">
+            <img src="../assets/search.png" alt="">
+
+        </div>
+    </div>
+</div>
+</template>
+<script>
+import TitleCom from '@/components/TitleCom.vue'
+import {defineComponent } from 'vue'
+export default defineComponent ({
+setup(){
+ return{}
+},
+components:{
+    TitleCom
+}
+})
+</script>
+<style lang="scss" scoped>
+.btn{
+    font-size: 20px;
+    font-weight: 600;
+    color:cadetblue;
+    border:1px solid cadetblue;
+    cursor: pointer;
+    padding: 10px 5px;
+    border-radius: 5px;
+}
+.box{
+    width: 1000px;
+    margin: 0 auto;
+    box-shadow: 0 6px 18px #999;
+}
+</style>
